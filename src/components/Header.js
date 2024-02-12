e12f080f0beba145523dd5adfd740a92f685e9d7
0sugo <josugo26@gmail.com>
2024-02-12 21:33:28 +0300

    Add custom header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-[#283387] h-14 px-4'>
+        <button className='bg-white text-[#283387] px-2 mt-3 py-1 float-right rounded-lg md:hidden'><a href='' className=' P-4 hover:bg-inherit rounded-lg' >Login</a></button>
+
+
+
+    </div>
+  )
+}
+
+export default Header
