a8f4b0cf500b80cec30a6393c8371477f53c379e
0sugo <josugo26@gmail.com>
2024-02-12 21:32:18 +0300

    Modify App.ss

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Output from './components/Output';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+      <Output />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
