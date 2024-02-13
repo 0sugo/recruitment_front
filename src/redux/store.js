7afcc25f90000ec4d9ea4a2af5a7015dbd56882d
0sugo <josugo26@gmail.com>
2024-02-13 23:31:09 +0300

    Modify  store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore} from '@reduxjs/toolkit';
 import JobsReducer from './Jobs/JobsSlice'
+import authReducer from './Registration/RegistrationSlice';
 
 const store = configureStore({
   reducer: {
     allJobs: JobsReducer,
-
+    auth: authReducer,
   },
 });
 export default store;
