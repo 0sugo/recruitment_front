29bafc41e192ae37021317a9c3ede87e5e918fd1
0sugo <josugo26@gmail.com>
2024-03-15 09:39:59 +0300

    Modify referees

--- src/components/Referees.js
+++ src/components/Referees.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tabular from './Tabular'
+import { useDispatch } from 'react-redux';
+import { submitReferees } from '../redux/Profile/refereesSlice';
 
 const Referees = () => {
+  const dispatch = useDispatch();
+  const userId = 129
+
+  const [formData, setFormData] = useState({
+    full_name: '',
+    position: '',
+    mobile_no: '',
+    email: '',
+    period: '',
+    full_name2: '',
+    position2: '',
+    mobile_no2: '',
+    email2: '',
+    period2: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(submitReferees({ userId, refereeData: formData }));
+  };
+
+
+
   return (
     <div className='mt-8'>
       <Tabular />
@@ -12,7 +42,7 @@ const Referees = () => {
         <div className="w-full lg:w-8/12 px-4 mx-auto mt-6 ">
           <div className="flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white border-0">
             <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-              <form >
+              <form onSubmit={handleSubmit}>
                 <h6 class="text-red-600 text-sm mt-3 mb-6 font-bold uppercase">
                   FIRST REFEREE
                 </h6>
@@ -20,9 +50,10 @@ const Referees = () => {
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                      Full names
+                        Full names
                       </label>
-                      <input type="text" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input type="text" name='full_name' value={formData.full_name}
+                        onChange={handleChange}required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
@@ -32,7 +63,8 @@ const Referees = () => {
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
                         Occupation
                       </label>
-                      <input type="email" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input type="text" name='position' value={formData.position}
+                        onChange={handleChange}required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
@@ -41,58 +73,34 @@ const Referees = () => {
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
                         Mobile No
                       </label>
-                      <input type="tel" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input type="tel" name='mobile_no' value={formData.mobile_no}
+                        onChange={handleChange} required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                       email
+                        email
                       </label>
-                      <input type="email" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input type="email" name='email' value={formData.email}
+                        onChange={handleChange} required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                       Period for which the referee has known you:
+                        Period for which the referee has known you:
                       </label>
-                      <input type="text" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
-                    </div>
-                  </div>
-
-                  {/* Start Date */}
-                  <div class="w-full lg:w-3/12 px-6">
-                    <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="startDate">
-                        Start Date
-                      </label>
-                      <input
-                        type="date"
-                        required
-                        id="startDate"
-                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      />
+                      <input type="number" name='period' value={formData.period}
+                        onChange={handleChange} required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
 
-                  {/* End Date */}
-                  <div class="w-full lg:w-3/12 px-6">
-                    <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="endDate">
-                        End Date
-                      </label>
-                      <input
-                        type="date"
-                        required
-                        id="endDate"
-                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      />
-                    </div>
-                  </div>
+
+
 
                 </div>
 
@@ -105,9 +113,10 @@ const Referees = () => {
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                      Full names
+                        Full names
                       </label>
-                      <input type="text" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input type="text" name='full_name2' value={formData.full_name2}
+                        onChange={handleChange}required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
@@ -117,7 +126,8 @@ const Referees = () => {
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
                         Occupation
                       </label>
-                      <input type="email" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input type="text" name='position2' value={formData.position2}
+                        onChange={handleChange} required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
@@ -126,56 +136,28 @@ const Referees = () => {
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
                         Mobile No
                       </label>
-                      <input type="tel" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input type="tel" name='mobile_no2' value={formData.mobile_no2}
+                        onChange={handleChange} required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                       email
+                        email
                       </label>
-                      <input type="email" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input type="email" name='email2' value={formData.email2}
+                        onChange={handleChange} required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
                       <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                       Period for which the referee has known you:
+                        Period for which the referee has known you ( In Yrs):
                       </label>
-                      <input type="text" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
-                    </div>
-                  </div>
-
-                  {/* Start Date */}
-                  <div class="w-full lg:w-3/12 px-6">
-                    <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="startDate">
-                        Start Date
-                      </label>
-                      <input
-                        type="date"
-                        required
-                        id="startDate"
-                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      />
-                    </div>
-                  </div>
-
-
-                  {/* End Date */}
-                  <div class="w-full lg:w-3/12 px-6">
-                    <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="endDate">
-                        End Date
-                      </label>
-                      <input
-                        type="date"
-                        required
-                        id="endDate"
-                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      />
+                      <input type="number" name='period2' value={formData.period2}
+                        onChange={handleChange} required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                     </div>
                   </div>
 
