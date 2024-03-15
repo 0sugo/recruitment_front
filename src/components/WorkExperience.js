3d831d8925b91729786fae79a8d7e0611508d4ad
0sugo <josugo26@gmail.com>
2024-03-15 09:42:03 +0300

    Modify work experience

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,7 +1,30 @@
-import React from 'react'
-import Tabular from './Tabular'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { submitWorkExperience } from '../redux/Profile/workExperienceSlice';
+import Tabular from './Tabular';
 
 const WorkExperience = () => {
+  const dispatch = useDispatch();
+  const userId = localStorage.getItem('userId');
+
+  const [formData, setFormData] = useState({
+    position: '',
+    job_group: '',
+    mcda: '',
+    start_date: '',
+    end_date: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(submitWorkExperience({ userId, workExperienceData: formData }));
+  };
+
   return (
     <div className='mt-8'>
       <Tabular />
@@ -10,70 +33,98 @@ const WorkExperience = () => {
 
       <section className="py-1 bg-blueGray-50">
         <div className="w-full lg:w-8/12 px-4 mx-auto mt-6">
-          <div className=" flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white border-0">
+          <div className="flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white border-0">
             <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-              <form>
-                <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+              <form onSubmit={handleSubmit}>
+                <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
                   SECTION ONE: Work Experience
                 </h6>
-                <div class="flex flex-wrap">
-                  <div class="w-full lg:w-6/12 px-6">
-                    <div class=" w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                      Designation/Position
+                <div className="flex flex-wrap">
+                  <div className="w-full lg:w-6/12 px-6">
+                    <div className="w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="designation">
+                        Designation/Position
                       </label>
-                      <input type="text" required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input
+                        type="text"
+                        name="position"
+                        value={formData.position}
+                        onChange={handleChange}
+                        required
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="w-full lg:w-6/12 px-6">
+                    <div className="w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="NCDA">
+                        Job Group
+                      </label>
+                      <input
+                        type="text"
+                        name="job_group"
+                        value={formData.job_group}
+                        onChange={handleChange}
+                        required
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      />
                     </div>
                   </div>
 
 
-                  <div class="w-full lg:w-6/12 px-6">
-                    <div class=" w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                  <div className="w-full lg:w-6/12 px-6">
+                    <div className="w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="NCDA">
                         NCDA
                       </label>
-                      <input  required type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                      <input
+                        type="text"
+                        name="mcda"
+                        value={formData.mcda}
+                        onChange={handleChange}
+                        required
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      />
                     </div>
                   </div>
 
-                  {/* Start Date */}
-                  <div class="w-full lg:w-6/12 px-6">
-                    <div class=" w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="startDate">
+                  <div className="w-full lg:w-6/12 px-6">
+                    <div className="w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="startDate">
                         Start Date
                       </label>
                       <input
                         type="date"
+                        name="start_date"
+                        value={formData.start_date}
+                        onChange={handleChange}
                         required
-                        id="startDate"
-                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       />
                     </div>
                   </div>
 
-
-                  {/* End Date */}
-                  <div class="w-full lg:w-6/12 px-6">
-                    <div class=" w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="endDate">
+                  <div className="w-full lg:w-6/12 px-6">
+                    <div className="w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="endDate">
                         End Date
                       </label>
                       <input
                         type="date"
+                        name="end_date"
+                        value={formData.end_date}
+                        onChange={handleChange}
                         required
-                        id="endDate"
-                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       />
                     </div>
                   </div>
-
                 </div>
 
-                <hr class="mt-6 border-b-1 border-blueGray-300" />
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
 
                 <button type='submit' className='bg-blue-600 px-5 py-2 rounded-lg hover:bg-blue-700 mt-2 text-white'>Submit</button>
-
-
               </form>
             </div>
           </div>
@@ -83,4 +134,4 @@ const WorkExperience = () => {
   )
 }
 
-export default WorkExperience
+export default WorkExperience;
