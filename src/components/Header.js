f4492859c21eccdb40638794c0f02d2fdcfc9658
0sugo <josugo26@gmail.com>
2024-03-15 09:37:00 +0300

    Modify Headers.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 const Header = () => {
+
+
+  const handleLogout = () => {
+    localStorage.removeItem('userId');
+    window.location.href = '/login';
+
+  };
+
   return (
     <div className='bg-[#283387] h-14 px-4'>
-        <button className='bg-white text-[#283387] px-2 mt-3 py-1 float-right rounded-lg md:hidden'><a href='' className=' P-4 hover:bg-inherit rounded-lg' >Login</a></button>
-
-
-
+      <button className='bg-white text-[#283387] px-2 mt-3 py-1 float-right rounded-lg '><a href='' className=' P-4 hover:bg-inherit rounded-lg' >Login</a></button>
+      <button className='bg-white text-[#283387] px-2 mt-3 py-1 float-right rounded-lg' onClick={handleLogout}>
+        <a href='/login' className='p-4 hover:bg-inherit rounded-lg'>Logout</a>
+      </button>
     </div>
   )
 }
