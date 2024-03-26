1454ae411feb61aaf87b934b9c740ac99f44b4db
0sugo <josugo26@gmail.com>
2024-03-26 23:41:48 +0300

    Update registerPage.js

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { registerUser } from '../redux/Registration/RegistrationSlice';
+import Output from './Output';
 
 const RegisterPage = () => {
   const dispatch = useDispatch();
-
-
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [job_id, setJob_id] = useState('');
   const [password, setPassword] = useState('');
@@ -21,64 +21,74 @@ const RegisterPage = () => {
       password,
     };
 
-    dispatch(registerUser(userData));
+    try {
+      await dispatch(registerUser(userData));
+      setTimeout(() => {
+        navigate('/login');
+      }, 5000);
+    } catch (error) {
+      console.error('Registration failed:', error);
+    }
   };
 
   return (
-    <div className='flex justify-center items-center'>
-      <div className="flex flex-col items-center mt-20 bg-gray-100">
+    <>
+      <Output />
+      <div className='flex justify-center items-center'>
+        <div className="flex flex-col items-center mt-20 bg-gray-100">
 
-        <form className="shadow-2xl rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-          <h2 className="font-bold mb-6">Register</h2>
+          <form className="shadow-2xl rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+            <h2 className="font-bold mb-6">Register</h2>
 
-          <div className="mb-4 flex flex-nowrap gap-4 items-baseline justify-end">
-            <label className="mb-2 text-sm">Email :</label>
-            <input
-              className="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-2/3 shadow appearance-none  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </div>
+            <div className="mb-4 flex flex-nowrap gap-4 items-baseline justify-end">
+              <label className="mb-2 text-sm">Email :</label>
+              <input
+                className="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-2/3 shadow appearance-none  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4 flex flex-nowrap gap-4 items-baseline justify-end">
-            <label className="mb-2 text-sm">Job Id :</label>
-            <input
-              className="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 w-2/3"
-              type="tel"
-              value={job_id}
-              onChange={e => setJob_id(e.target.value)}
-            />
-          </div>
+            <div className="mb-4 flex flex-nowrap gap-4 items-baseline justify-end">
+              <label className="mb-2 text-sm">Job Id :</label>
+              <input
+                className="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 w-2/3"
+                type="tel"
+                value={job_id}
+                onChange={e => setJob_id(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4 flex flex-nowrap gap-4 items-baseline justify-end">
-            <label className="mb-2 text-sm">Password :</label>
-            <input
-              className="border p-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-2/3"
-              type="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-          </div>
+            <div className="mb-4 flex flex-nowrap gap-4 items-baseline justify-end">
+              <label className="mb-2 text-sm">Password :</label>
+              <input
+                className="border p-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-2/3"
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4 flex flex-nowrap gap-4 items-baseline justify-end">
-            <label className="mb-2 text-sm">Confirm Password </label>
-            <input
-              className="border p-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-2/3"
-              type="password"
-              value={confirmPassword}
-              onChange={e => setConfirmPassword(e.target.value)}
-            />
-          </div>
+            <div className="mb-4 flex flex-nowrap gap-4 items-baseline justify-end">
+              <label className="mb-2 text-sm">Confirm Password </label>
+              <input
+                className="border p-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-2/3"
+                type="password"
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
+              />
+            </div>
 
-          <button className="shadow bg-[#283387] hover:bg-[#283387]focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Register</button>
-          <div class="md:w-2/3 mb-5"></div>
-          Have an account?
-          <Link to="/login" class="hover:text-[#283280] font-bold"> Login</Link>
-        </form>
+            <button className="shadow bg-[#283387] hover:bg-[#283387]focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Register</button>
+            <div class="md:w-2/3 mb-5"></div>
+            Have an account?
+            <Link to="/login" class="hover:text-[#283280] font-bold"> Login</Link>
+          </form>
 
+        </div>
       </div>
-    </div>
+    </>
 
 
   )
