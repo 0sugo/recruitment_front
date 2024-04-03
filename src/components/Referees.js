7792ff57c4ff213874d4a1610caafe596203d8d3
0sugo <josugo26@gmail.com>
2024-04-03 09:30:15 +0300

    Update referees

--- src/components/Referees.js
+++ src/components/Referees.js
@@ -39,20 +39,20 @@ const Referees = () => {
     <div className=''>
       {/* <Tabular /> */}
 
-      <h2 className='uppercase'>Referees</h2>
-
       <section className="py-1 ">
-        <div className="w-full lg:w-8/12 px-4 mx-auto mt-6 ">
+        <div className="w-full lg:w-10/12 px-4 mx-auto mt-6 ">
           <div className="flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white border-0">
             <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
               <form onSubmit={handleSubmit}>
                 <h6 class="text-red-600 text-sm mt-3 mb-6 font-bold uppercase">
-                  FIRST REFEREE
+                <span className='font-bold text-black'>SECTION seven: </span>FIRST REFEREE
+
+
                 </h6>
                 <div class="flex flex-wrap">
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
                         Full names
                       </label>
                       <input type="text" name='full_name' value={formData.full_name}
@@ -63,7 +63,7 @@ const Referees = () => {
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
                         Occupation
                       </label>
                       <input type="text" name='position' value={formData.position}
@@ -73,7 +73,7 @@ const Referees = () => {
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
                         Mobile No
                       </label>
                       <input type="tel" name='mobile_no' value={formData.mobile_no}
@@ -83,7 +83,7 @@ const Referees = () => {
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
                         email
                       </label>
                       <input type="email" name='email' value={formData.email}
@@ -93,8 +93,8 @@ const Referees = () => {
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                        Period for which the referee has known you:
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
+                        Period for which the referee has known you ( Yrs):
                       </label>
                       <input type="number" name='period' value={formData.period}
                         onChange={handleChange} required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
@@ -102,20 +102,19 @@ const Referees = () => {
                   </div>
 
 
-
-
-
                 </div>
 
                 <hr class="mt-6 border-b-1 border-blueGray-300" />
 
                 <h6 class="text-red-600 text-sm mt-3 mb-6 font-bold uppercase">
-                  SECOND REFEREE
+                <span className='font-bold text-black'>SECTION seven: </span> SECOND REFEREE
+
+
                 </h6>
                 <div class="flex flex-wrap">
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
                         Full names
                       </label>
                       <input type="text" name='full_name2' value={formData.full_name2}
@@ -126,7 +125,7 @@ const Referees = () => {
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
                         Occupation
                       </label>
                       <input type="text" name='position2' value={formData.position2}
@@ -136,7 +135,7 @@ const Referees = () => {
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
                         Mobile No
                       </label>
                       <input type="tel" name='mobile_no2' value={formData.mobile_no2}
@@ -146,7 +145,7 @@ const Referees = () => {
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
                         email
                       </label>
                       <input type="email" name='email2' value={formData.email2}
@@ -156,8 +155,8 @@ const Referees = () => {
 
                   <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                        Period for which the referee has known you ( In Yrs):
+                      <label class="block uppercase text-blueGray-600 text-xs  mb-2" htmlfor="grid-password">
+                        Period for which the referee has known you ( Yrs):
                       </label>
                       <input type="number" name='period2' value={formData.period2}
                         onChange={handleChange} required class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
