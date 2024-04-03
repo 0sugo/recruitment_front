e4df3e387acd032e14c54beaff185045d69a5332
0sugo <josugo26@gmail.com>
2024-04-03 09:57:00 +0300

    Update styles for leave form

--- src/components/LeaveForm.js
+++ src/components/LeaveForm.js
@@ -101,7 +101,7 @@ const LeaveForm = () => {
 
       <div className='max-w-3/4 mx-auto border-b border-l b-2 border-gray-200 text-sm'>
         <div className='flex mx-auto max-w-3/4 whitespace-nowrap gap-16 bg-gray-100'>
-        <div className='font-semibold text-sm flex gap-16 text-slate-600'>
+          <div className='font-semibold text-sm flex gap-16 text-slate-600'>
             <span className={`flex items-center p-3 cursor-pointer ${selectedComponent === 'LeaveReport' ? 'underline animate-bounce text-black' : ''}`} onClick={() => handleComponentSelect('LeaveReport')}>Leave Summary</span>
             <span className={`flex items-center p-3 cursor-pointer ${selectedComponent === 'PendingLeave' ? 'underline animate-bounce text-black' : ''}`} onClick={() => handleComponentSelect('PendingLeave')}>Pending Leave</span>
             <span className={`flex items-center p-3 cursor-pointer ${selectedComponent === 'ApprovedLeave' ? 'underline animate-bounce text-black' : ''}`} onClick={() => handleComponentSelect('ApprovedLeave')}>Approved Leave</span>
@@ -112,12 +112,12 @@ const LeaveForm = () => {
         <div className='content mt-4'>
           <div className='flex flex-col items-start px-4 mb-4'>
             <p className='mb-2 '>Leaves Taken already :</p>
-              <div className='flex justify-between w-full'>
-                <p className='shadow-slate-400 shadow-md p-2 rounded-md'>Annual leave: 10 / 45</p>
-                <p className='shadow-slate-400 shadow-md p-2 rounded-md'>Sick leave: 1 / 10</p>
-                <p className='shadow-slate-400 shadow-md p-2 rounded-md'>Compassionate leave: 0 / 10</p>
-                <p className='shadow-slate-400 shadow-md p-2 rounded-md'>Total: 11 / 65</p>
-              </div>
+            <div className='flex justify-between w-full'>
+              <p className='shadow-slate-400 shadow-md p-2 rounded-md'>Annual leave: 10 / 45</p>
+              <p className='shadow-slate-400 shadow-md p-2 rounded-md'>Sick leave: 1 / 10</p>
+              <p className='shadow-slate-400 shadow-md p-2 rounded-md'>Compassionate leave: 0 / 10</p>
+              <p className='shadow-slate-400 shadow-md p-2 rounded-md'>Total: 11 / 65</p>
+            </div>
           </div>
 
           {selectedComponent === 'LeaveReport' && <LeaveReport />}
@@ -138,9 +138,10 @@ const LeaveForm = () => {
           <section className="py-1 ">
             <div className="w-full lg:w-8/12 px-4 mx-auto mt-6 ">
               <div className="flex flex-col min-w-0  w-full mb-6 shadow-lg rounded-lg bg-white border-0">
-                <div className="flex items-start justify-between p-4 mb-4">
+                <h3 className='uppercase'>LEAVE APPLICATION FORM</h3>
+                <div className="flex items-start justify-between px-4 py-2 mb-2">
+                  <p></p>
 
-                  <h2 className='uppercase'>LEAVE APPLICATION</h2>
                   <button
                     className="text-gray-700 hover:text-gray-900 text-2xl focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -154,8 +155,8 @@ const LeaveForm = () => {
 
                       <div class="w-full lg:w-4/12 px-6">
                         <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                            Name
+                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
+                            Name :
                           </label>
                           <input type="text" name='full_name' value={formData.full_name} min={0} max={30}
                             required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
@@ -164,8 +165,8 @@ const LeaveForm = () => {
 
                       <div class="w-full lg:w-4/12 px-6">
                         <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                            Phone
+                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
+                            Phone :
                           </label>
                           <input type="tel" name='phone' value={formData.position}
                             required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
@@ -174,8 +175,8 @@ const LeaveForm = () => {
 
                       <div class="w-full lg:w-4/12 px-6">
                         <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                            Postal Address
+                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
+                            Postal Address :
                           </label>
                           <input type="text" name='postal_address' value={formData.position}
                             required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
@@ -184,8 +185,8 @@ const LeaveForm = () => {
 
                       <div class="w-full lg:w-6/12 px-6">
                         <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                            Designation
+                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
+                            Designation :
                           </label>
                           <input type="text" name='designation' value={formData.position}
                             required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
@@ -194,15 +195,15 @@ const LeaveForm = () => {
 
                       <div className="w-full lg:w-6/12 px-6">
                         <div className="w-full mb-3">
-                          <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="leave_days">
-                            During my Leave, My Salary should:
+                          <label className="block uppercase text-blueGray-600 text-xs mb-2 " htmlFor="leave_days">
+                            During my Leave, My Salary should :
                           </label>
                           <select
                             name="leave_days"
                             value={formData.leave_days}
 
                             required
-                            className="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                            className="border-0 px-2 py-2 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                           >
                             <option value="">Select one option</option>
                             <option value="a">Continue to be paid into my account</option>
@@ -214,8 +215,8 @@ const LeaveForm = () => {
 
                       <div class="w-full lg:w-6/12 px-6">
                         <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                            Leave Duration (days)
+                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
+                            Leave Duration (days) :
                           </label>
                           <input type="number" name='leave_days' value={formData.full_name}
                             required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
@@ -224,17 +225,17 @@ const LeaveForm = () => {
 
                       <div class="w-full lg:w-6/12 px-6">
                         <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                            Start Date
+                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
+                            Start Date :
                           </label>
                           <input type="date" name='start_date' value={formData.start_date}
-                            required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                            required class="border-0 px-2 py-2 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                         </div>
                       </div>
 
                       {/* <div class="w-full lg:w-6/12 px-6">
                     <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                      <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
                         Last Leave
                       </label>
                       <input type="date" name='last_leave_date' value={formData.start_date}
@@ -244,8 +245,8 @@ const LeaveForm = () => {
 
                       <div className="w-full mt-2 px-6 flex justify-center">
                         <div className="w-full max-w-md mb-3">
-                          <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="signature">
-                            Signature
+                          <label className="block uppercase text-blueGray-600 text-xs mb-2" htmlFor="signature">
+                            Signature :
                           </label>
                           <ReactSignatureCanvas
                             ref={signatureRef}
@@ -255,7 +256,7 @@ const LeaveForm = () => {
                             onEnd={() => console.log('User has finished signing.')}
                           />
                           <div className="flex justify-center">
-                            <button onClick={handleClear}>Clear Signature</button>
+                            <button onClick={handleClear} className='text-xs p-2 bg-neutral-50'>Clear Signature</button>
                           </div>
                           <p className="text-xs text-blueGray-500 mt-1 text-red-600">Please sign in the box above using your mouse or finger if on a touch device.</p>
                         </div>
