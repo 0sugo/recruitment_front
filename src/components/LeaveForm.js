b5badc77d0595dcaa2364faf564d9ec7e7f1242c
0sugo <josugo26@gmail.com>
2024-04-30 14:44:41 +0300

    Update status

--- src/components/LeaveForm.js
+++ src/components/LeaveForm.js
@@ -3,11 +3,9 @@ import React, { useEffect, useRef, useState } from 'react';
 import ReactToPrint from 'react-to-print';
 import coat from '../images/coat.png';
 import ReactSignatureCanvas from 'react-signature-canvas';
-import { useDispatch } from 'react-redux';
-import { submitForm } from '../redux/Leave/LeaveSlice';
-// import Output from './Output';
+import { useDispatch,useSelector } from 'react-redux';
+import { getLeaves, submitForm } from '../redux/Leave/LeaveSlice';
 import { HiOutlinePlus } from "react-icons/hi";
-
 import LeaveReport from './LeaveReport';
 import PendingLeave from './PendingLeave';
 import RejectedLeave from './RejectedLeave';
@@ -17,12 +15,23 @@ const LeaveForm = () => {
   const componentRef = useRef(null);
   const signatureRef = useRef();
   const dispatch = useDispatch();
+  const leaves = useSelector((state) => state.leave.leaves);
   const [showModal, setShowModal] = React.useState(false);
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
   const [selectedComponent, setSelectedComponent] = useState('LeaveReport');
+  const [formData, setFormData] = useState({});
+
   const handleComponentSelect = (componentName) => {
     setSelectedComponent(componentName);
   };
+
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}${month}${day}`;
+  };
+
 
 
   useEffect(() => {
@@ -33,66 +42,46 @@ const LeaveForm = () => {
     return () => clearInterval(timerID);
   }, []);
 
+  useEffect(()=>{
+    dispatch(getLeaves());
+  },[dispatch]);
+
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
   const formattedDateTime = currentDateTime.toLocaleString('en-US', options);
   const [signatureDataUrl, setSignatureDataUrl] = useState('');
 
 
-  const formData = {
-    pfNo: '123456',
-    date: '2024-03-10',
-    name: 'Kasyoki',
-    through: 'Emmanuel James',
-    designation: 'Manager',
-    phone: '0721234567',
-    days: 14,
-    leaveStartDate: '2024-03-15',
-    lastLeaveStartDate: '2023-03-15',
-    lastLeaveEndDate: '2023-03-29',
-    leaveAddress: '123 Bishop St, Nairobi',
-    salaryPaymentOption: 'b',
-    rejectedReason: 'Understaffed',
-    payrollAddress: '1234 - Absa Bank, Nairobi Branch',
-    payMonth: 'March 2024',
-    permissionDate: '2024-02-25',
-    headOfDepartmentApproval: 'Approved',
-    principalSecretaryApproval: 'Approved',
-    leaveBalance: 21,
-    resumeDate: '2024-03-30',
-    hrmDepartment: {
-      leaveBroughtForward: 5,
-      leaveDaysCurrentYear: 26,
-      totalLeaveDaysDue: 31,
-      daysAlreadyTaken: 10,
-    },
-  };
-
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const signatureData = signatureRef.current.toDataURL();
+    // const signatureData = signatureRef.current.toDataURL();
 
     const formData = {
-      full_name: e.target.full_name.value,
-      phone: e.target.phone.value,
-      postal_address: e.target.postal_address.value,
+      // full_name: e.target.full_name?.value,
+      // phone: e.target.phone?.value,
+      // postal_address: e.target.postal_address.value,
       designation: e.target.designation.value,
-      leave_days: e.target.leave_days.value,
-      start_date: e.target.start_date.value,
-      // last_leave_date: e.target.last_leave_date.value,
-      signature: signatureData,
+      leave_type: e.target.leave_type.value,
+      leave_address: e.target.leave_address.value,
+      salary_paid_to: e.target.salary_paid_to.value,
+      account_no: e.target.account_no?.value,
+      num_of_days: e.target.num_of_days.value,
+      leave_begins_on: e.target.leave_begins_on.value,
+      signed: 1,
+      // signature: signatureData,
+      date: new Date().toISOString().split('T')[0],
     };
 
+    console.log(formData);
     dispatch(submitForm(formData));
-    console.log(signatureData);
 
-    setSignatureDataUrl(signatureData);
+    // setSignatureDataUrl(signatureData);
   };
 
   const handleClear = () => {
     signatureRef.current.clear();
   };
+
 
   return (
     <div className='flex flex-col'>
@@ -140,8 +129,6 @@ const LeaveForm = () => {
               <div className="flex flex-col min-w-0  w-full mb-6 shadow-lg rounded-lg bg-white border-0">
                 <h3 className='uppercase'>LEAVE APPLICATION FORM</h3>
                 <div className="flex items-start justify-between px-4 py-2 mb-2">
-                  <p></p>
-
                   <button
                     className="text-gray-700 hover:text-gray-900 text-2xl focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -151,58 +138,55 @@ const LeaveForm = () => {
                 </div>
                 <div className="flex-auto px-4 lg:px-10 py-5 pt-0">
                   <form onSubmit={handleSubmit}>
-                    <div class="flex flex-wrap">
+                    <div className="flex flex-wrap">
 
-                      <div class="w-full lg:w-4/12 px-6">
-                        <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
-                            Name :
-                          </label>
-                          <input type="text" name='full_name' value={formData.full_name} min={0} max={30}
-                            required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
-                        </div>
-                      </div>
-
-                      <div class="w-full lg:w-4/12 px-6">
-                        <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
-                            Phone :
-                          </label>
-                          <input type="tel" name='phone' value={formData.position}
-                            required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
-                        </div>
-                      </div>
-
-                      <div class="w-full lg:w-4/12 px-6">
-                        <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
-                            Postal Address :
-                          </label>
-                          <input type="text" name='postal_address' value={formData.position}
-                            required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
-                        </div>
-                      </div>
-
-                      <div class="w-full lg:w-6/12 px-6">
-                        <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
+                      <div className="w-full lg:w-4/12 px-6">
+                        <div className="w-full mb-3">
+                          <label className="block uppercase text-black text-xs mb-2" htmlFor="grid-password">
                             Designation :
                           </label>
-                          <input type="text" name='designation' value={formData.position}
-                            required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                          <input type="text" name='designation' value={formData.designation} placeholder='e.g snr ICT officer'
+                            required className="border-0 px-2 py-2  text-black bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                         </div>
                       </div>
 
-                      <div className="w-full lg:w-6/12 px-6">
+                      <div className="w-full lg:w-4/12 px-6">
                         <div className="w-full mb-3">
-                          <label className="block uppercase text-blueGray-600 text-xs mb-2 " htmlFor="leave_days">
+                          <label className="block uppercase text-black text-xs mb-2" htmlFor="grid-password">
+                            Leave Address :
+                          </label>
+                          <input type="text" name='leave_address' value={formData.leave_address} placeholder='e.g p.o box 40326-100'
+                            required className="border-0 px-2 py-2  text-black bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                        </div>
+                      </div>
+
+
+
+                      <div className="w-full lg:w-4/12 px-6">
+                        <div className="w-full mb-3">
+                          <label className="block uppercase text-black text-xs mb-2" htmlFor="grid-password">
+                            Leave Type :
+                          </label>
+                          <select name='leave_type' value={formData.leave_type} required className="border-0 px-2 py-2  text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" >
+                            <option value="a">Select one option</option>
+                            <option value="4">Sick Leave</option>
+                            <option value="5">Maternity</option>
+                            <option value="6">Annual</option>
+                          </select>
+                        </div>
+                      </div>
+
+                      <div className="w-full lg:w-4/12 px-6">
+                        <div className="w-full mb-3">
+                          <label className="block uppercase text-black text-xs mb-2 " htmlFor="leave_days">
                             During my Leave, My Salary should :
                           </label>
                           <select
-                            name="leave_days"
-                            value={formData.leave_days}
+                            name="salary_paid_to"
+                            value={formData.salary_paid_to}
                             required
-                            className="border-0 px-2 py-2 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                            onChange={(e) => setFormData({ ...formData, salary_paid_to: e.target.value })}
+                            className="border-0 px-2 py-2  text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                           >
                             <option value="">Select one option</option>
                             <option value="a">Continue to be paid into my account</option>
@@ -212,39 +196,41 @@ const LeaveForm = () => {
                         </div>
                       </div>
 
-                      <div class="w-full lg:w-6/12 px-6">
-                        <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
+                      {formData.salary_paid_to !== 'a' && formData.salary_paid_to !== '' && (
+                        <div className="w-full lg:w-4/12 px-6">
+                          <div className="w-full mb-3">
+                            <label className="block uppercase text-black text-xs mb-2" htmlFor="grid-password">
+                              Account No :
+                            </label>
+                            <input type="number" name='account_no' value={formData.account_no} placeholder='e.g 123456789999'
+                              required className="border-0 px-2 py-2  text-black bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                          </div>
+                        </div>
+                      )}
+
+                      <div className="w-full lg:w-4/12 px-6">
+                        <div className="w-full mb-3">
+                          <label className="block uppercase text-black text-xs mb-2" htmlFor="grid-password">
                             Leave Duration (days) :
                           </label>
-                          <input type="number" name='leave_days' value={formData.full_name}
-                            required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                          <input type="number" name='num_of_days' value={formData.num_of_days} placeholder='e.g 15 days'
+                            required className="border-0 px-2 py-2  text-black bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                         </div>
                       </div>
 
-                      <div class="w-full lg:w-6/12 px-6">
-                        <div class="w-full mb-3">
-                          <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
-                            Start Date :
+                      <div className="w-full lg:w-4/12 px-6">
+                        <div className="w-full mb-3">
+                          <label className="block uppercase text-black text-xs mb-2" htmlFor="grid-password">
+                            Leave start Date :
                           </label>
-                          <input type="date" name='start_date' value={formData.start_date}
-                            required class="border-0 px-2 py-2 placeholder-blueGray-300 text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                          <input type="date" name='leave_begins_on' value={formData.leave_begins_on}
+                            required className="border-0 px-2 py-2  text-slate-500 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
                         </div>
                       </div>
 
-                      {/* <div class="w-full lg:w-6/12 px-6">
-                    <div class="w-full mb-3">
-                      <label class="block uppercase text-blueGray-600 text-xs mb-2" htmlfor="grid-password">
-                        Last Leave
-                      </label>
-                      <input type="date" name='last_leave_date' value={formData.start_date}
-                        required class="border-0 px-2 py-2 placeholder-blueGray-300 text-blueGray-600 bg-[#f8f6f6] rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
-                    </div>
-                  </div> */}
-
-                      <div className="w-full mt-2 px-6 flex justify-center">
+                      {/* <div className="w-full mt-2 px-6 flex justify-center">
                         <div className="w-full max-w-md mb-3">
-                          <label className="block uppercase text-blueGray-600 text-xs mb-2" htmlFor="signature">
+                          <label className="block uppercase text-black text-xs mb-2" htmlFor="signature">
                             Signature :
                           </label>
                           <ReactSignatureCanvas
@@ -259,11 +245,11 @@ const LeaveForm = () => {
                           </div>
                           <p className="text-xs text-blueGray-500 mt-1 text-red-600">Please sign in the box above using your mouse or finger if on a touch device.</p>
                         </div>
-                      </div>
+                      </div> */}
 
                     </div>
 
-                    <hr class="mt-4 border-b-1 border-blueGray-300" />
+                    <hr className="mt-4 border-b-1 border-blueGray-300" />
                     <div className='flex gap-2 justify-center'>
                       <button type='submit' className='bg-blue-600 px-5 py-2 rounded-lg hover:bg-blue-700 mt-2 text-white'>Apply</button>
                       {/* <ReactToPrint trigger={() => <button className='bg-green-600 rounded-lg px-5 py-2 mt-2'>Print Form</button>} content={() => componentRef.current} /> */}
