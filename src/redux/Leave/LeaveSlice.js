5dee036ae8e67503eaaa36e5561969aa0e357e27
0sugo <josugo26@gmail.com>
2024-05-17 14:47:25 +0300

    Update Leave Slice

--- src/redux/Leave/LeaveSlice.js
+++ src/redux/Leave/LeaveSlice.js
@@ -8,38 +8,94 @@ const initialState = {
 };
 
 const url = `https://sir.magicalfurnitures.co.ke/api`;
+const token = localStorage.getItem('token');
+const userId = localStorage.getItem('userId');
 
-export const submitForm = createAsyncThunk('leave/submitForm', async (formData) => {
+export const getFormData = createAsyncThunk('leave/personalDetails', async (formData) => {
   try {
-  const userId = localStorage.getItem('userId');
-    const response = await axios.post(`${url}/createLeaveApplication/${userId}`, formData);
+    const config = {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const response = await axios.get(`${url}/applyLeave`, config);
+
+    console.log(response.data);
     return response.data;
   } catch (error) {
     throw new Error('Failed to submit form');
   }
 });
 
-export const getLeaves = createAsyncThunk('leave/GetLeaves', async() => {
+export const submitForm = createAsyncThunk('leave/submitForm', async (formData) => {
   try {
-  const userId = localStorage.getItem('userId');
-    // const response = await axios.get(`${url}/getMyLeaves/${userId}`);
-    const response = await axios.get(`${url}/getMyLeaves/110`);
+    const config = {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const response = await axios.post(`${url}/submitDetails`, formData, config);
+
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to submit form');
+  }
+});
+
+// Get my all Leaves
+export const getLeaves = createAsyncThunk('leave/GetLeaves', async () => {
+  try {
+    const config = {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      }
+    };
+    console.log('fired');
+
+    const response = await axios.get(`${url}/listLeaves`,config);
+    console.log(response.data);
+
     return response.data;
   } catch (error) {
     throw new Error('Failed to retrieve Leaves');
   }
 });
 
-export const getAllLeaves = createAsyncThunk('leave/GetLeaves', async() => {
+// Get all Leaves Super User
+export const getAllLeaves = createAsyncThunk('leave/GetAllLeaves', async () => {
   try {
-  const userId = localStorage.getItem('userId');
-    // const response = await axios.get(`${url}/getMyLeaves/${userId}`);
-    const response = await axios.get(`${url}/getMyLeaves/110`);
+    const config = {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      }
+    };
+
+    const response = await axios.get(`${url}/listLeaves`,config);
+
     return response.data;
   } catch (error) {
     throw new Error('Failed to retrieve Leaves');
   }
 });
+
+
+
+// To be deleted
+
+// export const getAllLeaves = createAsyncThunk('leave/GetLeaves', async () => {
+//   try {
+//     const userId = localStorage.getItem('userId');
+//     const response = await axios.get(`${url}/getMyLeaves/${userId}`);
+//     // const response = await axios.get(`${url}/getMyLeaves/110`);
+//     return response.data;
+//   } catch (error) {
+//     throw new Error('Failed to retrieve Leaves');
+//   }
+// });
 
 
 const formSlice = createSlice({
@@ -73,6 +129,21 @@ const formSlice = createSlice({
     });
 
     builder.addCase(getLeaves.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    });
+    builder.addCase(getFormData.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+
+    builder.addCase(getFormData.fulfilled, (state, action) => {
+      state.loading = false;
+      state.error = null;
+      state.personal = action.payload;
+    });
+
+    builder.addCase(getFormData.rejected, (state, action) => {
       state.loading = false;
       state.error = action.error.message;
     });
