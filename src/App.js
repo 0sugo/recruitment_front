8876922a5a1b232031a211c363d809d500057bfd
0sugo <josugo26@gmail.com>
2024-05-30 09:24:18 +0300

    Update font size

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App text-xs">
       {/* {userId === 'admin' ? <Admin /> : <Jobs />} */}
       {/* <LoginPage /> */}
       {/* <Dashboard /> */}
