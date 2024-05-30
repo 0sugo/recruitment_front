413451fdc1ec416d77af260450ee02942270832c
kZilla <kzilla@vn-z.zilla>
2024-05-30 08:57:10 +0300

    Punguza percentages

--- src/components/LeaveReport.js
+++ src/components/LeaveReport.js
@@ -59,7 +59,7 @@ const LeaveReport = () => {
           <ClipLoader color={'#123abc'} loading={loading} size={90} />
         </div>
       ) : (
-        <div className="absolute top-[10%] left-1/2 transform -translate-x-1/2 w-full max-w-7xl bg-white shadow-lg rounded-lg overflow-hidden md:p-4 sm:w-[90%] xs:w-[95%]">
+        <div className="absolute top-[10%] left-1/2 transform -translate-x-1/2 w-full max-w-7xl bg-white shadow-lg rounded-lg overflow-hidden md:p-4 sm:w-[80%] xs:w-[90%]">
           <div className="p-4 border-b">
             <h2 className="text-xl font-semibold">My Applications</h2>
           </div>
